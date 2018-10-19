8c5d323bcd17b00da75a5c335b171fd7cb057a4f
alexandrumc <alexandru.cmilitaru@gmail.com>
2018-10-19 23:26:46 +0300

    Changed word

--- ctc-translation/config.py
+++ ctc-translation/config.py
@@ -1,11 +1,11 @@
 
-# Change with phase and round variables names
+# Modify with phase and round variables names
 variables = dict(
     phase="Insert PHASE variable name here",
     round="Insert ROUND variable name here"
 )
 
-# Change with phase and round fields names in the message structure
+# Modify with phase and round fields names in the message structure
 msg_structure_fields = dict(
     phase_field="Insert PHASE FIELD name here",
     round_field="Insert ROUND FIELD name here"
