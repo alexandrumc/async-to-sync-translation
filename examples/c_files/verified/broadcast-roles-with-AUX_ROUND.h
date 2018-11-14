13f94139a3e329b620b1d99082203ddac15e74d9
Cezara <cezarad1@dhcp143.dmi.ens.fr>
2018-11-14 14:11:19 +0100

    broadcast

---
+++ examples/c_files/verified/broadcast-roles-with-AUX_ROUND.h
@@ -0,0 +1,13 @@
+//
+//  broadcast-roles-with-AUX_ROUND.h
+//  
+//
+//  Created by C on 14/11/2018.
+//
+
+#ifndef broadcast_roles_with_AUX_ROUND_h
+#define broadcast_roles_with_AUX_ROUND_h
+
+#include <stdio.h>
+
+#endif /* broadcast_roles_with_AUX_ROUND_h */
