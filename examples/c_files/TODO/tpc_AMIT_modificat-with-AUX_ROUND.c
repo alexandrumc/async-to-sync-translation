6e691c67737c79459bb95553bad69fe426752fe1
Cezara <idragoi@MP-66144.local>
2018-11-23 16:28:08 +0100

    dsa

--- examples/c_files/TODO/tpc_AMIT_modificat-with-AUX_ROUND.c
+++ examples/c_files/TODO/tpc_AMIT_modificat-with-AUX_ROUND.c
@@ -1,7 +1,7 @@
 #include "stdlib.h"
 #include<stdio.h>
 #include<limits.h>
-#include "functions.h"
+//#include "functions.h"
 
 
 
@@ -34,32 +34,51 @@ void out(int v1, int v2);
 
 
 int in();
-
+//@ requires true;
+//@ ensures true;
 
 int rand_bool();
 
 
 msg *recv();
+//@ requires emp;
+/*@ ensures result->round |-> ?r &*& result->phase |-> ?p &*&
+            result->response |-> ?rsp &*& result->commit |-> ?c &*&
+            result->payload |-> ?pay &*& result->sender |-> ?s &*&
+    malloc_block_Msg(result) &*& INT_MIN < p &*& p < INT_MAX;
+@*/
 
 void dispose(msg *c);
-
+/*@
+ requires c->phase |-> _ &*& c->round |-> _ &*&
+ c->response |-> _ &*& c->commit |-> _ &*&
+ c->payload |-> _ &*& c->sender |-> _ &*& malloc_block_Msg(c);
+ @*/
+ //@ensures emp;
 
 void list_dispose(struct List *l);
+//@ requires lseg(l,0, ?v) &*& foreach(v,alloc_list());
+//@ ensures emp;
 
 
-void list_dispose_no_data(struct List *l);
-
-void dispose_list(list *c);
 
 void send(msg *message, int pid);
+//@ requires true;
+//@ ensures true;
 
 
 int leader(int phase, int net_size);
-
+//@ requires emp;
+//@ ensures 0<=result &*& result < net_size;
 
 int all_agree(struct List *l);
+//@ requires lseg(l,0, ?v)&*& foreach(v,alloc_list());
+//@ ensures lseg(l,0, v) &*& foreach(v,alloc_list()) &*& 0 <= result &*& result<=1;
 
 
+int reset_timeout();
+//@ requires emp;
+//@ ensures emp;
 
 
 
@@ -518,3 +537,60 @@ int main(int argc, char **argv) //@ : main
 	}
 
 }
+
+/*@
+
+predicate lseg(struct List* from, struct List* to, list<struct Msg *> v)
+requires from == to ? v == nil :
+                      from->next |-> ?next &*& from->message |-> ?msg &*& from->size |-> ?size &*&
+                      malloc_block_List(from) &*& lseg(next, to, ?nextv) &*& v == cons(msg,nextv);
+ 
+ @*/
+ 
+ /*@ predicate_ctor alloc_list()(struct Msg *msg) = msg == 0 ? true : msg->round |-> ?r &*& msg->phase |-> ?p &*&
+ 								  msg->sender |-> ?s &*& msg->response |-> ?e &*& 
+ 								  msg->payload |-> ?t &*& msg->commit |-> ?a &*& 
+ 								  malloc_block_Msg(msg);
+  
+  
+  @*/
+  /*@
+ predicate_ctor eq_list(int phase, int round)(struct Msg *msg) =
+ msg == 0 ? true :
+ msg->round |-> ?r &*& msg->phase |-> ?p &*& msg->sender |-> ?s &*& msg->response |-> ?e &*&
+ msg->payload |-> ?t &*& msg->commit |-> ?a &*& malloc_block_Msg(msg)&*&
+ (phase == p) &*& (round == r);
+ 
+ predicate mbox_tag_EQ(struct List *mbox, int phase, int round) =
+ lseg(mbox,0,?v) &*& foreach(v,eq_list(phase,round));
+ @*/
+ 
+ /*@
+ 
+ lemma void lemma_EQ_list_to_alloc_list(struct List* mbox, int phase, int round)
+ requires lseg(mbox,0, ?v) &*& foreach(v,eq_list(phase,round));
+ ensures lseg(mbox,0, v) &*&  foreach(v,alloc_list());
+ {
+ 
+ open lseg(mbox,0,v);
+ open foreach(v,eq_list(phase,round));
+ if (v == nil) {
+ close lseg(0,0,nil);
+ close foreach(nil,alloc_list());
+ }else{
+ 
+ open eq_list(phase,round)(head(v));
+ close alloc_list()(head(v));
+ lemma_EQ_list_to_alloc_list(mbox->next,phase,round);
+ 
+ open lseg(mbox->next,0,tail(v));
+ close lseg(mbox->next,0,tail(v));
+ open foreach(tail(v),alloc_list());
+ close foreach(tail(v),alloc_list());
+ 
+ close lseg(mbox,0,cons(head(v),tail(v)));
+ close foreach(cons(head(v),tail(v)),alloc_list());
+ }
+ }
+ @*/
+
