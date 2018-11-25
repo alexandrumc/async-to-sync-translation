518df44309cd822873d1992eaa98b82a92e34622
alexandrumc <alexandru.cmilitaru@gmail.com>
2018-11-25 14:48:53 +0200

    Refactored example

--- examples/inputs-async/Chandra-Toueg-consensus.c
+++ examples/inputs-async/Chandra-Toueg-consensus.c
@@ -421,8 +421,12 @@ int main(int argc, char**argv) //@ : main
             }else{
                 if (mbox!=0 && mbox->size >= (n + 1) / 2) {
                     //@ assert eq_val_list_pred( phase, round,mbox);
-                    if(all_ack(mbox)) ack =1;
-                    else ack = -1;
+                    if(all_ack(mbox)) {
+                        ack =1;
+                    }
+                    else {
+                        ack = -1;
+                    }
                 }
             }
         }
@@ -512,7 +516,6 @@ int main(int argc, char**argv) //@ : main
         mbox = NULL;
         break;
         
-    }else {//@ leak eq_max_val_list_pred(mbox);
     }
     
     
