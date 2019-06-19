1075ca49b16bc9aeba003a13cd7a7b50d45c7b52
Patricio Inzaghi <patricio.inzaghi@gmail.com>
2019-06-19 16:27:47 +0200

    First version compiling

--- examples/inputs-async/PBFT-ViewChange.c
+++ examples/inputs-async/PBFT-ViewChange.c
@@ -2,6 +2,10 @@
 #include<stdio.h>
 #include<limits.h>
 #include<stdbool.h>
+#include<time.h> 
+
+
+#define TIMEOUT	3
 
 typedef struct Msg {
 	int round;
@@ -16,100 +20,59 @@ typedef struct Msg {
 	struct List * preprepared_set;
 } msg;
 
-
-/*@
- predicate msg_pred(struct Msg * msg) =
- msg == 0? true : msg->round |-> _ &*& msg->ballot |-> _ &*& malloc_block_Msg(msg);
- @*/
-
-enum round_typ {ViewChange_ROUND, NewView_ROUND, AUX_ROUND} ;
-
 typedef struct List{
     msg * message;
     struct List * next;
     int size;
 } list;
 
-/*@
- // Predicate describing a complete linked list
- predicate list_pred(struct List* n;) =
- n == 0 ?
- true
- :
- n->message |-> ?msg &*& n->size |-> ?size &*& n->next |-> ?next &*& malloc_block_List(n) &*& n!=next 
- &*& msg->round |-> _ &*& msg->ballot |-> _ &*& msg->leader |-> _ &*& malloc_block_Msg(msg) &*& list_pred(next);
- @*/
+enum round_typ {ViewChange_ROUND, NewView_ROUND, AUX_ROUND} ;
 
+time_t current_time;
+ 
+bool timeout(){
+    double diff_t;
+    time_t now;
+    time(&now);
+    
+    diff_t = difftime(now, current_time);
+    
+    if(diff_t >= TIMEOUT) {
+        printf("TIMEOUT! %ld\n", (long)diff_t);
+        return true;
+    }else {
+        return false;   
+    }
+}
 
+void reset_timeout(){
+    time(&current_time);
+    printf("Reset time = %ld\n", (long)current_time);
+}
 
-/*@
- // Predicate describing a linked list of size one whose only value is vbigger or equal to max
- predicate mbox_geq_1(int max, struct List* l, int len) =
- (l == 0 && len == 0) ?
- true
- :
- len == 1 &*& l->message|-> ?msg &*& l->next |-> NULL &*& l->size |-> 1 &*& malloc_block_Msg(msg) &*& msg->round |-> ?v1 &*& msg->ballot |-> ?v2 &*& msg->leader |-> ?s2  &*& malloc_block_List(l) &*& max <= v2 ?
- true : false ;
- @*/
+bool newview_signed(){
+    return true;
+}
 
-
- 
- /*@
- // Predicate describing the min of a complete linked list
- predicate eq_val_list_pred(int val, int val2, struct List* n;) =
- n == 0 ?
- true
- :
- n->message |-> ?msg &*& msg->ballot |-> ?v &*& msg->leader |-> ?leader &*& msg->round |-> ?r &*& malloc_block_Msg(msg) &*& malloc_block_List(n) &*& n->next |-> ?next &*& n->size |-> ?s &*&
- n!=next &*&  val== v &*&  val2== r &*& eq_val_list_pred(val,val2, next);
- @*/
- 
- 
- /*@
- // Lemma for converting a full max list predicate into a list predicate
- lemma void eq_val_list_pred_to_list_pred_lemma(struct List *l)
- requires eq_val_list_pred( ?val, ?val2, l);
- ensures list_pred(l);
- {
- open eq_val_list_pred(val,val2, l);
- if (l != 0) {
- 
- eq_val_list_pred_to_list_pred_lemma(l->next);
- 
- // We need the next two lines to let VeriFast know that l != l->next
- open list_pred(l->next);
- close list_pred(l->next);
- close list_pred(l);
- }
- else close list_pred(0);
- }
- @*/
- 
- 
-int timeout();
-//@ requires emp;
-//@ ensures emp;
-
-int reset_timeout();
-//@ requires emp;
-//@ ensures emp;
+int primary(int n, int view){
+    return view % n;
+}
 
 void out(int v1, int v2);
-//@ requires true;
-//@ ensures true;
 
-int all_same(list *mbox, int leader);
-//@ requires true;
-//@ ensures true;
+bool all_same(list *mbox, int view){
+    return true;
+}
 
-void send(msg* message, int pid);
-//@ requires true;
-//@ ensures true;
+void send(msg* message, int pid){
+    printf("Sending message\n");
+}
+
 
 msg* recv()
-//@ requires emp;
-//@ ensures result->round |-> ?v1 &*& result->ballot |-> ?v &*& result->leader |-> ?leader &*& malloc_block_Msg(result) &*& INT_MIN <v &*& v < INT_MAX;
 {
+    printf("Waiting for messages\n");
+    
     msg* m = (msg *)malloc(sizeof(msg));
     if(m == 0) {
     abort();
@@ -120,35 +83,32 @@ msg* recv()
 
 
 void dispose(msg* c)
-//@ requires c->ballot |-> _ &*& c->round |-> _ &*& c->leader |-> _ &*& malloc_block_Msg(c);
-//@ ensures emp;
 {
     free(c);
 }
 
+void list_dispose(list* l)
+{
+    list* current = l;  
+    list* next;  
+      
+    while (current != NULL)  
+    {  
+        next = current->next;  
+        dispose(current->message);
+        free(current);  
+        current = next;  
+    }  
+          
+    l = NULL;  
+}
 
-void list_dispose(struct List *l);
-//@ requires list_pred(l);
-//@ ensures emp;
-
-void list_dispose1(struct List *l);
-//@ requires  mbox_geq_1(?v,l,?s);
-//@ ensures emp;
-
-void dispose_list(list* c);
-//@ requires  c->message |-> _ &*& c->next |-> _ &*& c->size |-> _ &*& malloc_block_List(c);
-//@ ensures emp;
-
-
-
-int coord(int net_size);
-//@ requires emp;
-//@ ensures 0<=result &*& result < net_size;
 
 int main(int argc, char **argv)//@ : main
 //@ requires true;
 //@ ensures true;
 {
+	reset_timeout();
     
     int n = argc;
     int to_all = n+1;
@@ -156,46 +116,39 @@ int main(int argc, char **argv)//@ : main
     enum round_typ round;
     int view;
     int pid,leader;
-    list *mbox = NULL;
+    list* mbox = NULL;
     list* mbox_new = NULL;
     msg* m = NULL;
     view = 0;
-    //@ int old_ballot = ballot-1;
 
-   //@ enum round_typ old_round = round;
     while(true)
-        //@ invariant ((old_ballot<ballot && round == NewBallot_ROUND));
     {
-        //@ old_ballot = ballot;
         round = ViewChange_ROUND;
-        //@ old_round = round;
-        //@ assert ((old_ballot<ballot) || (old_ballot==ballot && old_round <=round));
-			
-		  while(true){	
-        		if (timeout())
-        		{
-	            m = (msg *) malloc(sizeof(msg));
-	            if(m==0) {
-	            abort();
-	            }
-	            m->view = view;
-	            m->round = ViewChange_ROUND;
-	            
-	            //@assert(m->ballot == ballot && m->round == round);
-	            send(m, to_all);
-	            dispose(m);
-	            m = NULL;
-	            break;
-        		}
-     	  }
-        reset_timeout();
         mbox = NULL;
-        //@ int len = 0;
+	   
+	    // Everyone is watching for a timeout to occur
+        while(true){	
+            if (timeout())
+            {
+                m = (msg *) malloc(sizeof(msg));
+                if(m==0) {
+                abort();
+                }
+                m->view = view;
+                m->round = ViewChange_ROUND;
+
+                send(m, to_all);
+                dispose(m);
+                m = NULL;
+                break;
+            }
+        }
         
+        // We detected a timeout, we wait for a certificate from the other replicas
         while(true)
-            //@ invariant mbox==NULL && len ==0;
-        {
+        {   
             m = recv();
+            // Received a valid message
             if (m != NULL && m->view>=view && m->round == ViewChange_ROUND){
                 mbox_new = (list*) malloc(sizeof(list));
                 if(mbox_new==0) {
@@ -205,105 +158,54 @@ int main(int argc, char **argv)//@ : main
                 if(mbox!=0){mbox_new->size = mbox->size + 1;}
                 else { mbox_new->size =1 ;}
                 mbox_new->next = mbox;
-                //@ len =1;
+
                 mbox = mbox_new;
-                //@ close mbox_geq_1(ballot,mbox,1);
-            }
-            else {
-            	//@ close mbox_geq_1(ballot,NULL,0);
-            	free(m);
-            	}
-            //@ open mbox_geq_1(ballot,mbox,len);
-            if(mbox != NULL && mbox->size ==1 && mbox->next == NULL){
-           	 //@ close mbox_geq_1(ballot, mbox,1);
-           	 break;
-            }
-            if (timeout()){
-                //@ close mbox_geq_1(ballot, mbox,0);
-                break;}
+                
+                // If I receive 2f messages...
+                if(mbox!=NULL && mbox->size >= 2*(n + 1) / 3 && all_same(mbox,m->view) ) {
+                    break;
+                }
+
+            }else{ dispose(m); }
+
         }
-    
-       	 //@ assert mbox_geq_1(ballot,mbox,len);
-       	 //@ open mbox_geq_1(ballot,mbox,len);
-         if(mbox!=NULL && mbox->size ==1 && mbox->next == NULL){
-           	
-           //@ close mbox_geq_1(ballot,mbox,1);
-           //@ assert mbox_geq_1(ballot,mbox,1);
-           //@ open mbox_geq_1(ballot,mbox,1);
-           //@  old_ballot = ballot;
-             ballot = mbox->message->ballot;
-            //@ assert ((old_ballot<=ballot && old_round ==round));
-             leader = mbox->message->leader;
-            //@ close mbox_geq_1(old_ballot,mbox,1);
+        
+        // A timeout was properly certified, the new primary advertise the NewView to others
+        list_dispose(mbox);
+        mbox=NULL;
+        
+        if(primary(n,m->view)==pid) {
             
-           
-            //@ old_round = round;
-            round = AckBallot_ROUND;
-            //@ assert ((old_ballot<ballot) || (old_ballot==ballot && old_round <=round));
-           
-            //@ old_ballot = ballot;
+            printf("I am %i, the primary of the new view\n", pid);
+            
             m = (msg *) malloc(sizeof(msg));
-            if (m==0) {abort();}
-            m->ballot = ballot;
-            m->round = AckBallot_ROUND;
-            m->leader = leader;
-             
-            //@assert(m->ballot == ballot && m->round == round);
+            if(m==0) {
+            abort();
+            }
+            m->view = view;
+            m->round = NewView_ROUND;
+
             send(m, to_all);
-            
             dispose(m);
             m = NULL;
             
-            list_dispose1(mbox);
-            reset_timeout();
-            mbox = NULL;
+        }else{
+         
             while(true)
-            //@ invariant eq_val_list_pred(ballot,round,mbox);
             {
-                //@ open eq_val_list_pred(ballot,round,mbox);
                 m = recv();
-                if (m != NULL && m->ballot == ballot && m->round == round){
-                    mbox_new = (list*) malloc(sizeof(list));
-                    if(mbox_new==0) {abort(); }
-                    mbox_new->message =m;
-                    if(mbox!=0) {mbox_new->size = mbox->size + 1;}
-                    else {mbox_new->size =1 ;}
-                    mbox_new->next = mbox;
-                    mbox = mbox_new;
-                }
-                else {free(m);};
-                if (timeout()) {
+                // Received a valid confirmation of the NewView
+                if (m != NULL && m->view>=view && m->round == NewView_ROUND && newview_signed(m)){
                     break;
-                }
-                if(mbox != NULL && mbox->size > n/2){break;}
-                
-                //@ close eq_val_list_pred(ballot,round,mbox);
-            }
+                }else{ dispose(m); }
+    
+            }   
         
-            if (mbox != NULL && mbox->size > n/2){
-            if (all_same(mbox,leader)==1){
-                //@ assert eq_val_list_pred(ballot,round,mbox);
-                // log[ballot] = leader;
-                out(ballot, leader);
-            }
-            }
-            //@ close eq_val_list_pred(ballot,round,mbox);
-            //@ eq_val_list_pred_to_list_pred_lemma(mbox);
-            
-            if(mbox!=0) {
-            list_dispose(mbox);
-            }
-         }else{
-         	//@close mbox_geq_1(ballot,mbox,len);
-             list_dispose1(mbox);
-             mbox=NULL;
-         }
-       
-        //@ old_ballot = ballot;
+        }
+
         view++;
-       //@  old_round = round;
-        round = ViewChange_ROUND;
-        //@ assert ((old_ballot<ballot) || (old_ballot==ballot && old_round <=round));
-    }
+        reset_timeout();
+
+    }   
     return 1;
 }
