2b95c1f4965d3cf03f736761bd2110b2bb732a56
DanRadulescu962 <dan.radulescu@stud.acs.upb.ro>
2019-06-22 19:37:26 +0200

    Added regression script. run-translation restores config.py after running on all examples.

---
+++ regression_script.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# This script runs ATHOS on all examples and checks results against references
+# Run: ./regression_script.sh y/n y/n
+# First y/n: keep results or no after execution
+# second y/n: run with Verifast or no
+
+if [ $# -ne 2 ]; then
+	echo "Wrong number of parameters"
+	exit 1
+fi
+
+disVerif=0
+keep=0
+
+# Check input
+if [ "$1" == "y" ]; then
+	keep=1
+elif [ "$1" != "n" ]; then
+	echo "Illegal first parameter"
+	exit 1
+fi
+
+if [ "$2" == "y" ]; then
+	disVerif=1
+elif [ "$2" != "n" ]; then
+	echo "Illegal second parameter"
+	exit 1
+fi
+
+if [ $disVerif -eq 1 ]; then
+	command="./run-translation -disableVerifast -allExamples"
+else
+	command="./run-translation -allExamples"
+fi
+
+echo "Creating results folder"
+
+$command 2>/dev/null 1>/dev/null
+
+echo "Start output checking:"
+
+overall=1
+
+FILES=./results/*.sync.c
+
+for f in $FILES
+do
+
+	IFS='/' read -ra NAMES <<<$f
+	filename=${NAMES[2]}
+
+	IFS='.' read -ra NAMES <<<$filename
+	algo_name=${NAMES[0]} 
+	
+	diff "results/$filename" "examples/outputs-sync/$filename" >/dev/null 2>/dev/null
+	
+	if [ $? -ne 0 ]; then
+		tput setaf 1; echo "Algorithm $algo_name has wrong output"
+		overall=0
+	else
+		tput setaf 2; echo "Algorithm $algo_name is ok"
+	fi
+done
+
+tput sgr0
+echo ""
+
+if [ $overall -eq 1 ]; then
+	echo "All files OK"
+else
+	echo "Outputs differ. Check implementation"
+fi
+
+if [ $keep -eq 0 ]; then
+	rm -rf results
+fi
